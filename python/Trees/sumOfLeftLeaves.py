9812794ed932ee87d0f89ccecb1edf0418c2662f
GayatriHungund81194 <gayatri811.gh@gmail.com>
2019-04-17 16:46:38 -0700

    sum of left leaves

--- python/Trees/sumOfLeftLeaves.py
+++ python/Trees/sumOfLeftLeaves.py
@@ -39,23 +39,23 @@ class Node:
     def sumOfLeftLeaves(self,root):
 
         res = 0
+        
         if root is not None:
             if self.isLeaf(root.left):
                 res = res + root.left.data
             else:
                 res = res + self.sumOfLeftLeaves(root.left)
             res = res+self.sumOfLeftLeaves(root.right)
+            
         return res
 
-root = Node(20) 
-root.left = Node(9) 
-root.right = Node(49) 
-root.right.left = Node(23)         
-root.right.right = Node(52) 
-root.right.right.left = Node(50) 
-root.left.left = Node(5) 
-root.left.right = Node(12) 
-root.left.right.right = Node(12) 
+root = Node(1) 
+root.left = Node(2) 
+root.right = Node(3) 
+root.left.left = Node(4)         
+root.left.right = Node(5) 
+root.left.right.left = Node(6) 
+
 #root.printTree()
 s=root.sumOfLeftLeaves(root)
 print("Sum is:",s)
